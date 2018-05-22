3d2c3f2a93a3f0ceb580af271f23d0ce66bee5c7
aodom3 <aodom3@gmail.com>
2018-05-22 15:21:01 -0400

    reverted back, added deadpool, worked javascript

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let words = ["marvel","vanessa","pizza","tacos","golden girls","unicorn","chimichanga"];
+// words being used
+let words = ["vanessa","pizza","tacos","golden girls","unicorn","chimichanga"];
+// random word chosen from group of words
+let secretWord = words[Math.floor(Math.random()*words.length];
+// blank "_" that is shown on the screen to be populated
+let ansArray = (for i=0; i < words.length; i++) {
+    ansArray[] = "_"
+}
+let remainingGuesses < 6;
 
-// $().button('toggle')
