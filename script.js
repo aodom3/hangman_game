72d7af9b292ead66c89bc2fe472225ead26de198
aodom3 <aodom3@gmail.com>
2018-05-22 16:36:31 -0400

    added document ready javascript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+$(document).ready()
+
+
+
+// alphabet
 let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 // words being used
@@ -10,7 +15,9 @@ let ansArray = (for i=0; i < words.length; i++) {
 }
 let remainingGuesses < 6;
 
-function
+$function(){
+    $('letters').on('click' function(light))
+}
 
 function
 
