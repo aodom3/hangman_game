f22b63f15fbe278211d1711ea6ed6dd3d98ea9a9
aodom3 <aodom3@gmail.com>
2018-05-22 16:19:53 -0400

    added letters to javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 // words being used
 let words = ["vanessa","pizza","tacos","golden girls","unicorn","chimichanga"];
 // random word chosen from group of words
@@ -8,3 +10,8 @@ let ansArray = (for i=0; i < words.length; i++) {
 }
 let remainingGuesses < 6;
 
+function
+
+function
+
+function
