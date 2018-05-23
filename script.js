fd14f037b54ea86d7510c4fe2a8520f1e544d1b7
aodom3 <aodom3@gmail.com>
2018-05-23 14:18:39 -0400

    removed mute button, score, tries left into string

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // // once the page loads up
 // $(document).ready(function(){
+
+// --------------------words and word bank-------------------------------------------
+
 // // words being used
 const words = ["pizza", "golden girls", "unicorn", "chimichanga"];
+
 // // random word chosen from the array of words
 let secretWord = words[Math.floor(Math.random() * words.length)];
+
 // // blank "_" that is shown on the screen to be populated
 //has to match the letters;loops to same length as the words
 const ansArray = [];
@@ -11,9 +16,15 @@ for (let i = 0; i < words.length; i++) {
 	ansArray[i] = "_";
 }
 
+//resets the page if player wants to
 function myFunction() {
 	location.reload()
 }
+
+
+
+
+
 // let remainingGuesses < 6;
 // $function(){
 //     $('letters').on('click' function(light))
