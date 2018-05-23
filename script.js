f1c4018e3cf5634e9ee671653062d6146860fed5
aodom3 <aodom3@gmail.com>
2018-05-23 11:27:29 -0400

    cleaned up js

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-// once the page loads up
-
-$(document).ready(function(){
- 
-
-    }
-}
 
 
+// // once the page loads up
 
-// alphabet
-let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// $(document).ready(function(){
+//     $("button").click(function() [
+//         $
+//     ]
+//     }
+// }
 
-// words being used
-let words = ["vanessa","pizza","tacos","golden girls","unicorn","chimichanga"];
-// random word chosen from group of words
-let secretWord = words[Math.floor(Math.random()*words.length];
-// blank "_" that is shown on the screen to be populated
-let ansArray = (for i=0; i < words.length; i++) {
-    ansArray[] = "_"
-}
-let remainingGuesses < 6;
 
-$function(){
-    $('letters').on('click' function(light))
-}
+// // words being used
+// let words = ["vanessa","pizza","tacos","golden girls","unicorn","chimichanga"];
+// // random word chosen from group of words
+// let secretWord = words[Math.floor(Math.random()*words.length];
+// // blank "_" that is shown on the screen to be populated
+// function words(){
 
-function
+// }
 
-function
+
+// let ansArray = (for i=0; i < words.length; i++) {
+//     ansArray[] = "_"
+// }
+// let remainingGuesses < 6;
+
+// $function(){
+//     $('letters').on('click' function(light))
+// }
+
+// function gameEnd(win)
+// if (win)
+// else
+
+// function
