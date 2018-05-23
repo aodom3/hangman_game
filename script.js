16aebc85724dc754c9dbe19846f0bf6acef2aa1d
aodom3 <aodom3@gmail.com>
2018-05-23 14:04:51 -0400

    reset button working, took out stuck button

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
-
-
 // // once the page loads up
-
 // $(document).ready(function(){
-//     $("button").click(function() [
-//         $
-//     ]
-//     }
-// }
-
-
 // // words being used
-// let words = ["vanessa","pizza","tacos","golden girls","unicorn","chimichanga"];
-// // random word chosen from group of words
-// let secretWord = words[Math.floor(Math.random()*words.length];
+const words = ["pizza", "golden girls", "unicorn", "chimichanga"];
+// // random word chosen from the array of words
+let secretWord = words[Math.floor(Math.random() * words.length)];
 // // blank "_" that is shown on the screen to be populated
-// function words(){
+//has to match the letters;loops to same length as the words
+const ansArray = [];
+for (let i = 0; i < words.length; i++) {
+	ansArray[i] = "_";
+}
 
-// }
-
-
-// let ansArray = (for i=0; i < words.length; i++) {
-//     ansArray[] = "_"
-// }
+function myFunction() {
+	location.reload()
+}
 // let remainingGuesses < 6;
-
 // $function(){
 //     $('letters').on('click' function(light))
 // }
-
 // function gameEnd(win)
 // if (win)
 // else
-
-// function
+// }
